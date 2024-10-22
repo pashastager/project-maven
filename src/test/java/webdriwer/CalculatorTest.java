cf1ca85b89138ce4317c974422b4345c199f78ea
pavel <pashastager@gmail.com>
2024-10-22 22:24:29 +0300

    edit femaleGender

--- src/test/java/webdriwer/CalculatorTest.java
+++ src/test/java/webdriwer/CalculatorTest.java
@@ -31,6 +31,12 @@ public class CalculatorTest {
         WebElement inputWeightWebElement = webDriver.findElement(inputWeightBy);
         inputWeightWebElement.sendKeys("80");
 
+        //нажатие на чекбокс "М"
+        String femaleGenderXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[5]/td[2]/input[2]";
+        By femaleGenderBy = By.xpath(femaleGenderXpath);
+        WebElement femaleGenderWebElement = webDriver.findElement(femaleGenderBy);
+        femaleGenderWebElement.click();
+
         //нажатие на кнопку рассчитать
         String xpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
         By by = By.xpath(xpath);
